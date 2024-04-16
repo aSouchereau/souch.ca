5109a4c978181ea04ed384f02ff5727364970695
aSouchereau <dev@souch.ca>
2024-04-16 17:09:31 -0400

    Add navlist component

---
+++ src/components/NavList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ul>
+    <li>
+      <RouterLink to="/">Home</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/gallery">Gallery</RouterLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+</style>

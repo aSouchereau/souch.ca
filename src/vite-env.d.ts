c30e1023ab448c3fdab809c378196362468d2619
aSouchereau <dev@souch.ca>
2024-05-07 11:10:11 -0400

    Add env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUB_LYCHEE_KEY: string,
+    readonly VITE_LYCHEE_BASE_URL: string,
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}

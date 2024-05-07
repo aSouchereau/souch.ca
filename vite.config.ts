5ae04bbb2ea3511069ff5c17a1ea7baadf017e37
aSouchereau <dev@souch.ca>
2024-05-07 14:43:33 -0400

    Add vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as process from "node:process";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+        resolve: {
     alias: [{ find: "@", replacement: "/src" }],
+  },
+    server: {
+      proxy: {
+        '/api/' : {
+          target: env.VITE_LYCHEE_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization' : `Bearer ${env.VITE_PUB_LYCHEE_KEY}`,
+          }
+
+        }
+      },
+    }
   }
+
 })

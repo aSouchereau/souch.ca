1b24569416535e2ad40babb12676cc64be0ea602
aSouchereau <dev@souch.ca>
2024-05-08 11:56:15 -0400

    Add album page

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import Index from "./components/Pages/Index.vue";
 import Gallery from "./components/Pages/Gallery.vue";
+import Album from "@/components/Pages/Album.vue";
 
 const routes = [
     { path: '/', component: Index },
     { path: '/gallery', component: Gallery},
+    { path: '/album', component: Album },
 ];
 export const createApp = ViteSSG(
     App,

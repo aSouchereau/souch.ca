35a59004b47288c0569e9efef9e1dbd633fcacde
aSouchereau <dev@souch.ca>
2024-05-09 18:52:04 -0400

    Fix photo composable

--- src/composables/api/Photo.ts
+++ src/composables/api/Photo.ts
@@ -1,16 +1,17 @@
-import {AfterFetchContext, useFetch} from "@vueuse/core";
+import {AfterFetchContext, useFetch, UseFetchOptions} from "@vueuse/core";
 
 export function useImage(photoId: string | undefined)  {
-    const options = {
-        body: {
-            photoId: photoId
-        },
+    const options: UseFetchOptions = {
         afterFetch(ctx: AfterFetchContext) {
             ctx.data = ctx.data.size_variants
             return ctx;
         },
     }
-    const { isFetching, isFinished, error, data } = useFetch("/api/Photo::get", options).post().json();
+    const { isFetching, isFinished, error, data } = useFetch("/api/Photo::get", options)
+        .post({
+            photoId: photoId
+        },)
+        .json();
 
     return { isFetching, isFinished, error, data };
 }

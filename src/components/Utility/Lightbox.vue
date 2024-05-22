d1863817501e533dd0a2ac83c40004c8c39fd308
aSouchereau <dev@souch.ca>
2024-05-21 20:16:45 -0400

    Add fancybox component

---
+++ src/components/Utility/Lightbox.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import {onMounted, onUnmounted, onUpdated, ref} from "vue";
+import {Fancybox} from "@fancyapps/ui";
+import '@fancyapps/ui/dist/fancybox/fancybox.css'
+
+  const props = defineProps({
+    options: Object,
+  });
+
+  const container = ref<HTMLElement | null>(null);
+
+  onMounted(() => {
+    Fancybox.bind(container.value, '[data-fancybox]', {
+      ...(props.options || {}),
+    });
+  });
+
+  onUpdated(() => {
+    Fancybox.unbind(container.value);
+    Fancybox.close();
+
+    Fancybox.bind(container.value, '[data-fancybox]', {
+      ...(props.options || {}),
+    });
+  });
+
+  onUnmounted(() => {
+    Fancybox.destroy();
+  });
+</script>
+
+<template>
+  <div ref="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+:global(.fancybox__caption) {
+  text-align: center;
+}
+:global(.fancybox__caption .copy-notice) {
+  display: block;
+  font-size: 0.75rem;
+  color: grey;
+}
+</style>

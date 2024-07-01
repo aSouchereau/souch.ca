33a0825196ffd825eeae97e14a51b2cd492ee4c9
aSouchereau <dev@souch.ca>
2024-07-01 11:28:03 -0400

    Made section mobile responsive

--- src/components/Pages/Index/SubHeadlineSection.vue
+++ src/components/Pages/Index/SubHeadlineSection.vue
@@ -21,18 +21,17 @@ section#sub-headline {
   align-items: center;
   justify-content: center;
   width: 100%;
-  min-height: 200px;
+  min-height: 100px;
 }
 .rule {
-  display: block;
-  width: 60%;
+  display: none;
 }
 .sub-headline-content {
   position: absolute;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  min-height: 200px;
+  min-height: 100px;
   top: 0;
   padding: 0 2rem;
   font-size: 2rem;
@@ -40,5 +39,19 @@ section#sub-headline {
 }
 .sub-headline-content h2 {
   margin: 0 0 0.5rem 0;
+}
+
+@media only screen
+and (min-width: 650px) {
+  .sub-headline-content {
+    min-height: 200px;
+  }
+  section#sub-headline {
+    min-height: 200px;
+  }
+  .rule {
+    display: block;
+    width: 60%;
+  }
 }
 </style>

eca117a2559366e97d122c59752bb4e8d8ccd352
alexsouchereau <W0632604@myscc.ca>
2024-07-06 19:39:03 -0400

    Add carousel component

---
+++ src/components/Content/Carousel.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Carousel } from "@fancyapps/ui";
+import {ref} from "vue";
+
+const props = defineProps({
+  options: Object,
+  id: String,
+  photos: Object,
+});
+
+const container = ref<HTMLElement | null>(null);
+const options = props.options;
+
+new Carousel(container, options);
+</script>
+
+<template>
+  <div class="f-carousel" ref="container">
+    <div class="f-carousel__slide" v-for="photo in props.photos" :key="photo.id">
+      <img :src="photo.src" :alt="photo.caption">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

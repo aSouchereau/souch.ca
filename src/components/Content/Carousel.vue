97533a8dce16082f44192261d2d9fad53d2f2064
aSouchereau <dev@souch.ca>
2024-07-10 14:03:59 -0400

    Add view image button

--- src/components/Content/Carousel.vue
+++ src/components/Content/Carousel.vue
@@ -5,6 +5,7 @@ const { Carousel } = pkg;
 import "@fancyapps/ui/dist/carousel/carousel.css";
 
 import {onMounted, onUnmounted, ref} from "vue";
+import ActionButton from "@/components/Content/ActionButton.vue";
 
 const props = defineProps({
   options: Object,
@@ -25,31 +26,56 @@ onUnmounted(() => {
   carousel = null;
 });
 
+function navigateToImage(destination: string) {
+console.log(destination);
+}
+
 </script>
 
 <template>
   <div class="f-carousel" ref="container" id="cardSlider">
     <div class="f-carousel__viewport">
-      <div class="f-carousel__slide" v-for="photo in props.photos" :key="photo.id">
+      <div class="f-carousel__slide" v-for="photo in props.photos" :key="photo.id" @click="navigateToImage('/album/' + photo.albumId + '#f-' + photo.id)">
         <img :src="photo.thumb" :alt="photo.caption">
+        <div class="img-nav">
+          <ActionButton :destination="'/album/' + photo.albumId + '#f-' + photo.id" class="view-btn">View</ActionButton>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.img-nav {
+  position: absolute;
+  visibility: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  background: rgba(var(--black),0.5);
+  width: 100%;
+  height: 100%;
+}
+
 .f-carousel__slide {
   width: 100%;
+  &:hover {
+    .img-nav {
+      visibility: visible;
+      cursor: pointer;
+    }
+  }
 }
 
 .f-carousel__slide img {
+  display: block;
   max-width: 100%;
 }
 
 #cardSlider {
-  --f-carousel-slide-width: 100%;
-  --f-carousel-spacing: 0;
-  --f-carousel-slide-padding: 0 0.5rem;
+  --f-carousel-spacing: 16px;
+  --f-carousel-slide-padding: 0;
 
 
   --f-button-bg: rgb(var(--off-white));
@@ -72,7 +98,7 @@ onUnmounted(() => {
 
 @media screen and (min-width: 1024px) {
   .f-carousel__slide {
-    width: calc(100% / 3);
+    width: calc((100% - 32px) / 3);
   }
 
   #cardSlider {
